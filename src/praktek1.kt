af3894cf83e34055c2db3fc3e5d8400eb7c71791
GirasDragneel <girassinakti11@gmail.com>
2022-03-22 09:21:19 +0700

    first commit

---
+++ src/praktek1.kt
@@ -0,0 +1,17 @@
+fun main(){
+  var  namalengkap: String = "budianto"
+    var alamat : String
+    alamat = "yogyakarta"
+    var umur = 22
+  namalengkap = "ganjar"
+
+  println("masukkan umur")
+  var nama = readLine()
+
+var umur2 = Integer.valueOf(umurInput) + 2
+  var umur3 = umurInput!!.toInt() +5
+  println("nama anda $nama")
+  println("umur anda$umurInput")
+  println("umur anda 2 tahun mendatang : $")
+
+}
